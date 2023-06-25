d2180c0edd6114172e2444f6b505a327952f5515
Gustavo Gomes Contiero <64373903+gcontiero11@users.noreply.github.com>
2023-06-25 15:09:39 -0300

    Update Species_KNN.py

--- Flower Species- Identifier/Species_KNN.py
+++ Flower Species- Identifier/Species_KNN.py
@@ -5,7 +5,9 @@ class Species:
     petal_length = 0
     petal_width = 0
 
+
 def Open_file(file_name):
+    # Open the file and return a list of flowers with the data separated into classes
     flowers = []
     arq = open(f"{file_name}" , "r")
     for line in arq:
@@ -20,7 +22,9 @@ def Open_file(file_name):
     arq.close()
     return flowers
 
+
 def position_biggest_number(arr):
+    # Returns the position of the largest number in a list
     biggest_number = arr[0]
     position = 0
     for i in range(len(arr)):
@@ -29,16 +33,10 @@ def position_biggest_number(arr):
             position = i
     return position
 
-def show_file(arr):
-    for elem in arr:
-        print(f"Sepal Length: {elem.sepal_length}")
-        print(f"Sepal Width: {elem.sepal_width}")
-        print(f"Petal Length: {elem.petal_length}")
-        print(f"Petal Width: {elem.petal_width}")
-        print(f"Name: {elem.name}")
 
 def calculating_distance(flower,list,k):
-    minor_distance = []
+    # Calculate the closest species for a given unknown flower
+    closest_species = []
     species = []
     sepal_l1 = flower.sepal_length
     sepal_w1 = flower.sepal_width
@@ -53,32 +51,28 @@ def calculating_distance(flower,list,k):
         distance = ((sepal_l1 - sepal_l2)**2 + (sepal_w1 - sepal_w2)**2 + (petal_l1 - petal_l2)**2 + (petal_w1 - petal_w2)**2) ** (1/2)
         if len(species) < k:
             species.append(elem.name)
-            minor_distance.append(distance)
+            closest_species.append(distance)
         elif len(species) == k:
-            position = position_biggest_number(minor_distance)
-            biggest_value = minor_distance[position]
+            position = position_biggest_number(closest_species)
+            biggest_value = closest_species[position]
             if distance < biggest_value:
-                minor_distance[position] = distance
+                closest_species[position] = distance
                 species[position] = elem.name
-        #print(species)
-        #print(minor_distance)
     return species  
 
-def bigger_frequency(arr):
+
+def highest_frequency(arr):
+    # Determine the most frequent species in a list of species
     setosa_count = 0
     versicolor_count = 0
     virginica_count = 0
-    #print(arr)
     for elem in arr:
         if elem == 'setosa':
             setosa_count += 1
-            #print(f"setosa count: {setosa_count}")
         elif elem == 'versicolor':
             versicolor_count += 1
-            #print(f"versicolor count: {versicolor_count}")
         elif elem == 'virginica':
             virginica_count += 1
-            #print(f"virginica count: {virginica_count}")
     if setosa_count > versicolor_count and setosa_count > virginica_count:
         return "setosa"
     elif versicolor_count > setosa_count and versicolor_count > virginica_count:
@@ -86,17 +80,19 @@ def bigger_frequency(arr):
     else:
         return "virginica"
 
+
 def main():
+    # Main function of the program
     species = []
     flowers = Open_file("iris.data.csv")
     k = int(input("K value: "))
     file_name = input("Type the document name:")
-    #file_name = "iris.data_2.csv"
     Unknown_species = Open_file(file_name)
     i = 0
     for flower in Unknown_species:
         i += 1
         species = calculating_distance(flower,flowers,k)
-        flower.name = bigger_frequency(species)
-        print(f"flor {i} : {flower.name}")
+        flower.name = highest_frequency(species)
+        print(f"flower {i} : {flower.name}")
+
 main()
